caacf1d4f62625fdfe3e7fcdd48a702c362b1d92
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-12-01 17:22:35 -0300

    ex 01 - ap3 finalizado

--- data-structures/atividades-propostas/classes/article.cpp
+++ data-structures/atividades-propostas/classes/article.cpp
@@ -2,24 +2,52 @@
 #include <iostream>
 using namespace std;
 
+int Article::cont = 0;
+
 Article::Article(
     const string & a_name,
     unsigned long a_num,
     double a_price )
+    : name(a_name), num(a_num)
 {
-    name = a_name;
-    num = a_num;
     if(a_price < 0.0) {
         price = 0.0;
     } else { price = a_price; }
     
+    cout << "Artigo criado: " << name << endl;
+    ++cont;
+}
+
+Article::~Article() {
+    --cont;
+    cout << "Destrutor chamado para: " << name << endl;
+}
+
+Article global("Camisa", 5990, 59.9);
+
+int test() {
+
+    static Article static_test("Bermuda", 1235, 39.9);
+    Article local_test("casaco", 123, 59.9);
+
+    local_test.setPrice(70);
+
+    return 0;
 }
 
 int main() {
 
-    Article a("Camisa", 123456, 59.90);
-     
-     a.print();
+    Article main_article("Sapato", 45678, 120.20);
 
+    cout << "Chamando função test()..." << "\n";
+    test();
+
+    cout << "Chamando função test() pela segunda vez..." << "\n";
+    test();
+
+
+    cout << "Função print para objeto criado na main(): " << endl;
+    main_article.print();
+    
     return 0;
 }
--- data-structures/atividades-propostas/classes/article.h
+++ data-structures/atividades-propostas/classes/article.h
@@ -15,22 +15,31 @@ private:
     unsigned long num;
     double price;
 
+    static int cont;
+
 public:
     Article( const string&, unsigned long, double );
+    ~Article();
 
     string getName() { return name; } 
     double getPrice() { return price; }
 
     void print() {
-        cout << "Article: " << "\n\n";
+        cout << "Article: " << "\n";
         cout << "------------------------------" << endl;
         cout << "Name: " << name << endl;
         cout << "Number: " << num << endl;
         cout << "Price: " << price << endl;
         cout << "------------------------------" << endl;
-        system("pause");
+        // system("pause");
     } 
     
+    int getCont() { return cont; }
+    void setPrice( double valor ) { 
+        price = valor; 
+        cout << "Preço de " << name << " modificado para: " << price << endl;
+    }
+
 };
 
 #endif
